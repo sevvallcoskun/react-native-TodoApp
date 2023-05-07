d35ffbfc3a9efb2801a684dcf93d8aeee7f0d2e5
sevvallcoskun <asevval.coskun@istanbulticaret.edu.tr>
2023-05-07 15:13:00 +0300

    practise1

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName]=useState('Sevval');
+  const [age,setAge]=useState('21');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name:</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val)=>setName(val)} 
+      />
+      <Text>Enter age:</Text>
+      <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 32'
+        onChangeText={(val)=>setAge(val)} 
+      />
+      <Text>Name:{name}, Age:{age}</Text>
     </View>
   );
 }
@@ -17,4 +33,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200
+  }
 });
